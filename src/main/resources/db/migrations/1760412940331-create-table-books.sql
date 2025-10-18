2fc8e8760903b9d0b2e5a819feffa08f1893f434
Iuri Néviton <iurineviton@gmail.com>
2025-10-17 21:34:50 -0300

    fix: migration

--- src/main/resources/db/migrations/1760412940331-create-table-books.sql
+++ src/main/resources/db/migrations/1760412940331-create-table-books.sql
@@ -8,7 +8,7 @@ CREATE TABLE books
     id               UUID         NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
     title            VARCHAR(255) NOT NULL,
     isbn             VARCHAR(13)  NOT NULL,
-    number_of_pages  INTEGER      NOT NULL CHECK (number_of_pages > 0),
+    number_of_pages  INTEGER      NOT NULL CHECK (number_of_pages >= 1),
     publication_date DATE         NOT NULL,
     created_at       TIMESTAMPTZ  NOT NULL DEFAULT now(),
     updated_at       TIMESTAMPTZ  NOT NULL DEFAULT now(),
--- src/main/resources/db/migrations/1760709907363-create-table-readings.sql
+++ src/main/resources/db/migrations/1760709907363-create-table-readings.sql
@@ -7,10 +7,16 @@ CREATE TABLE readings
     user_id        UUID        NOT NULL REFERENCES users (id),
     book_id        UUID        NOT NULL REFERENCES books (id),
     status         VARCHAR(20) NOT NULL,
-    current_page   INTEGER     NOT NULL CHECK (current_page >= 0),
+    current_page   INTEGER     NOT NULL CHECK (current_page >= 1),
     started_at     TIMESTAMPTZ NOT NULL DEFAULT now(),
     last_readed_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     finished_at    TIMESTAMPTZ,
     created_at     TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at     TIMESTAMPTZ NOT NULL DEFAULT now()
+    updated_at     TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+    CONSTRAINT uk_readings_user_book UNIQUE (user_id, book_id)
 );
+
+CREATE INDEX idx_readings_user_id ON readings (user_id);
+CREATE INDEX idx_readings_book_id ON readings (book_id);
+CREATE INDEX idx_readings_status ON readings (status);
