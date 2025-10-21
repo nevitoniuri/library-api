e8c05477698525e937653b71940b522ea44b590a
Iuri Néviton <iurineviton@gmail.com>
2025-10-21 18:33:07 -0300

    fix: remove auditing

--- src/main/java/com/unichristus/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/unichristus/libraryapi/LibraryApiApplication.java
@@ -2,9 +2,7 @@ package com.unichristus.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class LibraryApiApplication {
 
