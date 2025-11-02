002b2ab5ebbbfca6438d9e5531fcbb399f266132
Iuri Néviton <iurineviton@gmail.com>
2025-11-02 17:41:54 -0300

    fix: logged user

--- src/main/java/com/unichristus/libraryapi/presentation/controller/UserController.java
+++ src/main/java/com/unichristus/libraryapi/presentation/controller/UserController.java
@@ -1,17 +1,18 @@
 package com.unichristus.libraryapi.presentation.controller;
 
-import com.unichristus.libraryapi.application.dto.request.UserUpdateRequest;
-import com.unichristus.libraryapi.application.dto.response.UserResponse;
 import com.unichristus.libraryapi.application.common.MapperUtil;
 import com.unichristus.libraryapi.application.common.ServiceURIs;
+import com.unichristus.libraryapi.application.dto.request.UserUpdateRequest;
+import com.unichristus.libraryapi.application.dto.response.UserResponse;
 import com.unichristus.libraryapi.domain.user.UserService;
-import com.unichristus.libraryapi.infrastructure.security.CustomUserDetails;
 import com.unichristus.libraryapi.infrastructure.security.LoggedUser;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,13 +23,16 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/me")
-    public UserResponse getMe(@LoggedUser CustomUserDetails userDetails) {
-        return MapperUtil.parse(userService.findUserByIdOrThrow(userDetails.getId()), UserResponse.class);
+    public UserResponse getMe(@LoggedUser UUID userId) {
+        return MapperUtil.parse(userService.findUserByIdOrThrow(userId), UserResponse.class);
     }
 
     @PutMapping("/me")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateMe(@RequestBody @Valid UserUpdateRequest dto, @LoggedUser CustomUserDetails userDetails) {
-        userService.updateUser(userDetails.getId(), dto.name(), dto.email(), dto.password());
+    public void updateMe(
+            @LoggedUser UUID userId,
+            @RequestBody @Valid UserUpdateRequest dto
+    ) {
+        userService.updateUser(userId, dto.name(), dto.email(), dto.password());
     }
 }
